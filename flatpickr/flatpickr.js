e2030f62c280a30c7cc4171b891ebd50b20a4729
hobbyscripterII <wruomma2@naver.com>
2025-05-28 11:04:48 +0900

    update: flatpickr 특정일자 강조 이벤트 추가

--- flatpickr/flatpickr.js
+++ flatpickr/flatpickr.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
-    const $date = $('#date');
+    const $date       = $('#date');
+    let highlightDate = ['2025-05-05', '2025-05-27', '2025-05-28'];
 
     $date.flatpickr({
-        defaultDate : 'today',
-        dateFormat  : 'Y-m-d'
+      dateFormat  : 'Y-m-d',
+      defaultDate : 'today',
+      onDayCreate : function(dObj, dStr, fp, dElem) {
+        const dateStr = dElem.dateObj.toISOString().slice(0, 10);
+
+        if (highlightDate.includes(dateStr)) {
+          dElem.classList.add("highlight-day");
+        }
+      }
     });
 });
